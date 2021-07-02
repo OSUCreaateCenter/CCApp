ccf0d1b369a9547bf2a485e5432b5198849f70e4
EC2 Default User <ec2-user@ip-172-31-55-19.ec2.internal>
2021-07-02 07:49:57 +0000

    beta

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_065225) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", default: "", null: false
-    t.integer "status", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
